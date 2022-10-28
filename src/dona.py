225202f14a1cc7b21f46b924d922fb0a9990d247
dona sameer <deesaam@gmail.com>
2022-10-28 23:33:06 +0300

    final commit

--- src/dona.py
+++ src/dona.py
@@ -3,11 +3,62 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import missingno as msno
+import plotly.graph_objects as go
+from matplotlib.pyplot import xlabel
+import plotly.express as px
+
 
 
 # %%
+df = pd.read_csv("../data/sdb_loans_cleaned.csv")
 
-df=pd.read_csv("../data/sdb_loans.csv")
+df_old# %%
+
+fig = px.histogram(df[(df.loan_type == 'تمويل نقل')], x="city", animation_frame= "year",
+ title="عدد قروض (تمويل النقل)",
+labels= {'city':'المدينه',
+        'loan_type':'نوع القرض'})
+fig.update_layout(yaxis_title= 'عدد القروض')
+fig.show()
+
+
+
+#%%
+
+df["loan_type"].unique()
+
+
+# %%
+df[(df.loan_type == 'تمويل نقل')]["city"].unique()
+# %%
+
+fig = px.histogram(df[(df.loan_type == 'تمويل الاعمال')], x="city", animation_frame= "year",
+ title="عدد قروض (تمويل الاعمال)",
+labels= {'city':'المدينه',
+        'loan_type':'نوع القرض'})
+fig.update_layout(yaxis_title= 'عدد القروض')
+fig.show()
+fig.update_layout(yaxis_range=[0,650])
+
+
+# %%
+fig = px.histogram(df[(df.loan_type == 'تمويل الافراد')], x="city", animation_frame= "year",
+ title="عدد قروض (تمويل الافراد)",
+labels= {'city':'المدينه',
+        'loan_type':'نوع القرض'})
+fig.update_layout(yaxis_title= 'عدد القروض')
+fig.show()
+
+#%%
+fig = px.histogram(df[(df.loan_type == 'التمويل الحر')], x="city", animation_frame= "year",
+ title="عدد قروض (التمويل الحر)",
+labels= {'city':'المدينه',
+        'loan_type':'نوع القرض'})
+fig.update_layout(yaxis_title= 'عدد القروض')
+
+
+
+fig.show()=pd.read_csv("../data/sdb_loans.csv")
 # %%
 
 #to get cols
@@ -45,7 +96,7 @@ df.replace("MALE", "ذكر", inplace=True)
 
 
 # %%
-df["age"].unique()
+df["loan_type"].unique()
 
 
 #%%
@@ -71,6 +122,7 @@ df_noage.gender.value_counts()
 df.gender.value_counts()
 
 # %%
+#converting date col type
 df.date = pd.to_datetime(df.date)
 # %%
 #extracting the year and month
@@ -82,8 +134,34 @@ df.sort_values("date", inplace = True)
 # %%
 msno.bar(df)
 # %%
-
+df["loan_type"].unique
 #df[(df.job.isnull()) | (df.job == "غير معرف" )].year.value_counts()
+
+df.columns
+# %%
+
+
+
+#%%
+x_labs = []
+for item in df.loan_type.unique():
+    x_labs.append(get_display(arabic_reshaper.reshape(item)))
+
+
+#%%
+sns.barplot(x="year", y="loan_type", data=df)
+
+
+
+
+
+
 
 
 # %%
+#df["year"].unique()
+#!pip install arabic_reshaper
+
+
+# %%
+
